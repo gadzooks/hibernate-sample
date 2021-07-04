07d44d86c1c579db0239f470e2a4ebe4d5e2459f
Amit Karwande <karwande@gmail.com>
2021-07-03 23:14:30 -0700

    adding Temporal to date/time columns for Entity

--- src/main/java/com/github/gadzooks/data/Application.java
+++ src/main/java/com/github/gadzooks/data/Application.java
@@ -8,6 +8,18 @@ import java.util.Date;
 public class Application {
     public static void main(String[] args) {
         Session session = HibernateUtils.getSessionFactory().openSession();
+//        createUser(session);
+
+
+        session.beginTransaction();
+        User dbUser = session.get(User.class, 1L);
+        System.out.println(dbUser);
+        session.getTransaction().commit();
+
+        session.close();
+    }
+
+    private static void createUser(Session session) {
         session.beginTransaction();
         User user = User.builder().
                 firstName("amit").
@@ -21,6 +33,5 @@ public class Application {
                 build();
         session.save(user);
         session.getTransaction().commit();
-        session.close();
     }
 }
--- src/main/java/com/github/gadzooks/data/entities/User.java
+++ src/main/java/com/github/gadzooks/data/entities/User.java
@@ -1,6 +1,7 @@
 package com.github.gadzooks.data.entities;
 
 import lombok.*;
+import org.hibernate.annotations.Formula;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -9,36 +10,47 @@ import java.util.Date;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
-@Setter(value = AccessLevel.PACKAGE)
+//@Setter(value = AccessLevel.PACKAGE)
+@Setter
 @Entity
 @Table(name = "FINANCES_USER")
+@Access(value = AccessType.FIELD)
+@ToString
+//NOTE : using lombok builder with JPA entity
+//https://stackoverflow.com/questions/34241718/lombok-builder-and-jpa-default-constructor/35602246#35602246
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
     private Long userId;
 
-    @Column(name = "FIRST_NAME")
+    @Column(name = "FIRST_NAME", nullable = false)
     private String firstName;
 
-    @Column(name = "LAST_NAME")
+    @Column(name = "LAST_NAME", nullable = false)
     private String lastName;
 
-    @Column(name = "BIRTH_DATE")
+    @Formula("CONCAT(first_name, ' ', last_name)")
+    private String fullName;
+
+    @Temporal(value = TemporalType.DATE)
+    @Column(name = "BIRTH_DATE", nullable = false)
     private Date birthDate;
 
-    @Column(name = "EMAIL_ADDRESS")
+    @Column(name = "EMAIL_ADDRESS", nullable = false)
     private String emailAddress;
 
+    @Temporal(value = TemporalType.TIMESTAMP)
     @Column(name = "LAST_UPDATED_DATE")
     private Date lastUpdatedDate;
 
     @Column(name = "LAST_UPDATED_BY")
     private String lastUpdatedBy;
 
-    @Column(name = "CREATED_DATE")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(name = "CREATED_DATE", updatable = false)
     private Date createdDate;
 
-    @Column(name = "CREATED_BY")
+    @Column(name = "CREATED_BY", updatable = false)
     private String createdBy;
 }
