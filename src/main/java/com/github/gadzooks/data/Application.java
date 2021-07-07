7d7ede15d478815fef6f008e5f7831fdf72945ae
Amit Karwande <karwande@gmail.com>
2021-07-06 20:37:19 -0700

    add slf4j

--- src/main/java/com/github/gadzooks/data/Application.java
+++ src/main/java/com/github/gadzooks/data/Application.java
@@ -4,11 +4,13 @@ import com.github.gadzooks.data.entities.Bank;
 import com.github.gadzooks.data.entities.User;
 import com.github.gadzooks.data.service.BankCrudService;
 import com.github.gadzooks.data.service.UserCrudService;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.SessionFactory;
 
 import static com.github.gadzooks.data.service.BankCrudService.createBank;
 import static com.github.gadzooks.data.service.UserCrudService.createUser;
 
+@Slf4j
 public class Application {
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
@@ -16,12 +18,12 @@ public class Application {
         BankCrudService bankService = new BankCrudService(sessionFactory);
         Bank bank = createBank();
         Bank savedBank = bankService.save(bank);
-        System.out.println("saved bank is : " + savedBank);
+        log.info("saved bank is : " + savedBank);
 
         UserCrudService userService = new UserCrudService(sessionFactory);
         User user = createUser();
         User savedUser = userService.save(user);
-        System.out.println("saved user is : " + savedUser);
+        log.info("saved user is : " + savedUser);
 
 //        User dbUser = session.get(User.class, 1L);
 //        System.out.println(dbUser);
